db6bab4dbc06c4f5c91ddfef1463091014f64344
Sisiphomafrika <sisiphomafrika99@gmail.com>
2024-01-10 17:21:33 +0200

    first

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
-import { createStore } from 'vuex'
-
+import { createStore } from 'vuex';
+import axios from 'axios';
 export default createStore({
   state: {
+    ResumeData: [],
+    ProjectData: [],
+    testimonialData: [],
   },
   getters: {
   },
   mutations: {
+    testimonialData(state, data) {
+      state.testimonialData = data;
+    },
   },
   actions: {
+    fetchData({ commit }) {
+      axios.get('http://localhost:3030/data') 
+        .then(response => {
+          console.log(response);
+          commit('testimonialData', response.data[2]);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    },
   },
   modules: {
-  }
-})
+  },
+});
